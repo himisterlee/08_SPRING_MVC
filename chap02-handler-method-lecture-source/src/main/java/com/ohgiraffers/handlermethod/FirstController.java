e708a2973b092c37461589f35b9bf5255d6008a1
[himisterlee] <[lejlej100418@gmail.com]>
2024-07-09 18:11:54 +0900

    chap02 완료

--- chap02-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
+++ chap02-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
@@ -1,15 +1,19 @@
 package com.ohgiraffers.handlermethod;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/first/*")
+@SessionAttributes("id")
 public class FirstController {
 
     /*
@@ -52,21 +56,133 @@ public class FirstController {
     * form의 name 속성값과 매개변수의 이름이 다른 경우 @RequestParam("이름") 설정하면 된다.
     *
     * 생략이 가능하다. 하지만 가독성을 위해서 써주는게 좋다.
+    *
+    * required속성 : 값이 들어오지 않아도 400 에러를 발생시키지 않는다. (default = true)
+    * defaultValue : 기본값을 설정
     * */
 
     @GetMapping("modify")
     public void modify() {}
 
     @PostMapping("modify")
-    public String modifyMenuPrice(Model model, @RequestParam String modifyName,
-                                  @RequestParam int modifyPrice) {
+    public String modifyMenuPrice(Model model, @RequestParam(required = false) String modifyName,
+                                  @RequestParam(defaultValue = "0") int modifyPrice) {
 
-        String message = modifyName + " 메뉴의 가격을 " + modifyPrice + "원으로 가격을 변경하였습니다.";
+        String message = modifyName + " 메뉴의 가격을 " + modifyPrice + "원으로 변경하였습니다.";
         System.out.println("message = " + message);
 
         model.addAttribute("message", message);
 
         return "first/messagePrinter";
+    }
+
+    /*
+    * 파라미터가 여러개인 경우 Map으로 한번에 처리할 수 있다.
+    * 이때, Map 의 키는 form의 name 속성값이 된다.
+    * */
+    @PostMapping("modifyAll")
+    public String modifyMenu(Model model, @RequestParam Map<String, String> parameters) {
+
+        String modifyName = parameters.get("modifyName2");
+        int modifyPrice = Integer.parseInt(parameters.get("modifyPrice2"));
+
+        String message = modifyName + " 메뉴의 가격을 " + modifyPrice + "원으로 변경하였습니다.";
+        System.out.println("message = " + message);
+
+        model.addAttribute("message", message);
+
+        return "first/messagePrinter";
+    }
+
+    // first/search
+    @GetMapping("search")
+    public void search() {}
+
+    /*
+    * @ModelAttribute
+    * - DTO 같은 모델을 커맨드 객체로 전달받는다.
+    *
+    * @ModelAttribute의 경우 커맨드객체를 생성하여 매개변수로 전달 해 준 뒤 인스턴스를 model에 담는다.
+    * 경우에 따라 폼에서 입력한 값을 다음 화면으로 바로 전달해야 할 경우가 발생하는데, 이때 유용하게 사용된다.
+    * @ModelAttribute("모델에 담을 key 값")을 지정 할 수 있으며
+    * 지정하지 않으면 타입의 앞글자를 소문자로 한 네이밍 규칙을 따른다.
+    * -> menuDTO
+    *
+    * 해당 어노테이션은 생략이 가능하지만 명시적으로 작성하는 것이 좋다.
+    * 이때 주의할점은 DTO와 입력받을 폼을 잘 맞춰줘야 한다.
+    * */
+    @PostMapping("search")
+    public String searchMenu(@ModelAttribute("menu") MenuDTO menu) {
+
+        System.out.println("menu = " + menu);
+
+        return "first/searchResult";
+    }
+
+    @GetMapping("login")
+    public void login() {}
+
+    /*
+    * 4-1 session 이용하기
+    * HttpSession을 매개변수로 선언하면 핸들러 메소드 호출 시 세션 객체를 넣어서 호출한다.
+    * */
+    @PostMapping("login1")
+    public String sessionTest1(HttpSession session, @RequestParam String id) {
+
+        System.out.println("session = " + session);
+        System.out.println("id = " + id);
+
+        session.setAttribute("id", id);
+
+        return "first/loginResult";
+    }
+
+    @GetMapping("logout1")
+    public String logoutTest1(HttpSession session) {
+
+        session.invalidate();
+
+        return "first/loginResult";
+    }
+
+
+    /*
+    * @SessionAttributes를 이용하여 session에 값 담기
+    * 클래스레벨에 @SessionAttributes 어노테이션을 이용하여 세션에 담을 key 값을 설정
+    * Model 영역에 해당 key로 값이 추가되는 경우 session에 자동으로 등록해준다.*/
+    @PostMapping("login2")
+    public String sessionTest2(Model model, @RequestParam String id) {
+        model.addAttribute("id", id);
+
+        return "first/loginResult";
+    }
+
+    @GetMapping("logout2")
+    public String logoutTest2(SessionStatus sessionStatus) {
+
+        // 현재 컨트롤러 세션에 저장된 모든 정보를 제거
+        // 개별 제거가 불가능
+        sessionStatus.setComplete();
+
+        return "first/loginResult";
+    }
+
+    @GetMapping("body")
+    public void body() {}
+
+    /*
+    * @RequestBody
+    * - 해당 어노테이션은 http 본문 자체를 읽는 부분을 모델로 변환시켜주는 어노테이션
+    * */
+    @PostMapping("body")
+    public void bodyTest(@RequestBody String body,
+                         @RequestHeader("content-type") String contentType,
+                         @CookieValue(value = "JSESSIONID", required = false) String sessionId) throws UnsupportedEncodingException {
+
+        System.out.println("contentType = " + contentType);
+        System.out.println("sessionId = " + sessionId);
+        System.out.println("body = " + body);
+        System.out.println("body = " + URLDecoder.decode(body, "UTF-8"));
     }
 
 }
