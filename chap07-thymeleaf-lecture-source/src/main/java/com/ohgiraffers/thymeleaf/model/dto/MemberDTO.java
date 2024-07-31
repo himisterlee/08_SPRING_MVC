7820defdbf9e71ef49d3d77480a252ad4b128f73
[himisterlee] <[lejlej100418@gmail.com]>
2024-07-31 15:02:00 +0900

    thymeleaf

---
+++ chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/model/dto/MemberDTO.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.thymeleaf.model.dto;
+
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class MemberDTO {
+
+    private String name;
+    private int age;
+    private char gender;
+    private String address;
+}
