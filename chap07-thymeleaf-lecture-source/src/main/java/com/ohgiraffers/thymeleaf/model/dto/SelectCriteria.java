2d968e71ba1d7989624338f18b66c448fc04d522
[himisterlee] <[lejlej100418@gmail.com]>
2024-08-01 16:04:01 +0900

    chap08 security 초입

--- chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/model/dto/SelectCriteria.java
+++ chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/model/dto/SelectCriteria.java
@@ -1,2 +1,15 @@
-package com.ohgiraffers.thymeleaf.model.dto;public class SelectCriteria {
+package com.ohgiraffers.thymeleaf.model.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class SelectCriteria {
+
+    private int startPage;
+    private int endPage;
+    private int pageNo;
 }
